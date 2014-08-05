dfede0afdb3e2970c0f8f1c1197d96cae6d6e252
Hugo Solar <hola@hugo.so>
2014-08-05 09:31:18 -0400

    first commit pix app

---
+++ app/js/app.js
@@ -0,0 +1,29 @@
+var pixObject = {
+	currentObject: null,
+	getCurrent : function() {
+		return this.currentObject;
+	},
+	setCurrent : function(obj) {
+		this.currentObject = obj;
+	}
+};
+var pix_icons = {
+	"think" : "pix-think",
+	"cube" : "pix-cube",
+	"mouse" : "pix-mouse",
+	"plus"	: "pix-plus",
+	"read"	: "pix-read",
+	"reload" : "pix-reload",
+	"keyboard": "pix-keyboard"
+	"click left" : "pix-click-left" ,
+	"click center" : "pix-click-center",
+	"keyboard type" : "keyboard-type",
+	"top left"	: "stack-top-left",
+	"circle dashed" : "pix-circle-dashed"
+	"square dashed"	: "pix-square-dashed"
+}
+(function ( $ ) {
+	$.fn.checkText = function(str) {
+
+	};
+}(jQuery));
