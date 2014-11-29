52ca2bb0d56609ee5e051f9df5eb7a1ab52c4ecc
Hugo Solar <hugo.solar@gmail.com>
2014-11-28 23:16:58 -0300

    corrección script

--- js/app.js
+++ js/app.js
@@ -36,19 +36,30 @@ var pixObject = {
 				$(this).replacePix($(this).text(), target);
 			});
 			$('.pix-steps').on('click','.btn-tools',function(event){
-				obj = $(this);
-				console.log(this.hash);
-
-				if (this.hash == '#add') {
-					obj.addNodeCurrent();
-				} else if ( this.hash == '#remove') {
-					obj.removeCurrentNode();
-				} else if ( this.hash == '#split') {
-					obj.splitCurrent()
-				}
+				$(this).clickTool();
 			});
 		}
 	}
+	$.fn.clickTool = function() {
+		obj = $(this);
+		switch(obj.attr('href')){
+			case '#add':
+				obj.addNodeCurrent();
+				break;
+			case '#remove':
+				obj.removeCurrentNode();
+			break;
+			case '#split':
+				obj.splitCurrent();
+			break;
+			case '#add-note':
+				obj.addNoteCurrent();
+			break;
+		}
+	}
+	/*
+	* Añade un nuevo score sin titulo via handlebars 
+	*/
 	$.fn.addScore= function(){
 		var pix_layout = $('#layout-score').html();
 		var step_template = $('#pix-step').html();
@@ -60,7 +71,9 @@ var pixObject = {
 		
 		return false;
 	}
-
+	/*
+	*	Ejecuta la acción de reemplazo (regex) del caracter Pix por el layout especificado
+	*/
 	$.fn.replacePix = function(str,target) {
 			var re = new RegExp("(pix[-][a-z])([a-z]+)([\w ]+)","gm");
 			var textarea = $(this).prev();
@@ -76,42 +89,24 @@ var pixObject = {
 		        $(target).html(str);
 	        }
 	}
-	// $.fn.flyLink = function(){
-	// 	var href = $(this).attr('href').replace('#','');
-	// 	console.log(href);
-	// 	if (href == 'split') {
-	// 		var split = $(this).parent();
-	// 		if (split.hasClass('split')) {
-	// 			split.removeClass('split');
-	// 			$(this).text('Split score');
-	// 		} else {
-	// 			split.addClass('split');
-	// 			$(this).text('Unsplit score');
-	// 		}
-
-	// 	}
-	// 	if (href == 'add-note') {
-	// 		console.log($(this).prev().find('.input-note'));
-	// 		var note = $(this).prev().find('.input-note');
-	// 		if (note.is(':visible')) {
-	// 			note.hide();
-	// 			$(this).text('Add note');
-	// 		} else {
-	// 			note.show();
-	// 			note.text('');
-	// 			$(this).text('Delete note');
-	// 		}
-	// 	}
-	// 	return false;
-	// }
+	/*
+	* Cambia la clase del ul contenedor generando un split
+	* TODO: hay que agregar el campo de texto para el título
+	*/
 	$.fn.splitCurrent = function() {
 		var obj = $(this);
 		obj.parent().next().toggleClass('split');
 	}
+	/*
+	* Elimina la columna contextual al botón
+	*/
 	$.fn.removeCurrentNode = function() {
 		var obj = $(this);
 		obj.parent().parent().remove();
 	}
+	/*
+	* Añade una columna despues de la actual
+	*/
 	$.fn.addNodeCurrent = function() {
 		var obj = $(this);
 		var step_template = $('#pix-step').html();
@@ -120,45 +115,20 @@ var pixObject = {
 		obj.parent().parent().after(column);
 
 	}
+	/*
+	*	Controla funciones de input como el avance del tab para crear una nueva columna
+	*/
 	$.fn.checkText = function(str, event) {
-		//console.log(str);
-		// $('.pix-replace').val(str);
-		// grep = '';
-		 var re =  new RegExp("(pix[-])([a-z]*)","gm");
-		 str = str.replace(re,'<i class="icon $1$2"></i>');
-		// var grep = $.grep(pix_icons,function(n,i){
-		// 	//console.log(n.text);
-		// 	//return n.text.match(regex); //regex que busca asociados... para el autocomplete
-		// 	return n.text===str;
-		// });
-		// //console.log(grep);
-		// if (grep.length == 1) {
-		// 	$('.pix-code').html(str.replace(regex,'<span class="pix-stack"><i class="pix '+grep.pop().id+'"></span>'));
-		// }
-
 		this.addNode = function(context) {
-			// var li = $('<li>').attr({class: 'pix-step col-sm-1 col-xs-3'});
-			// var fly_top = $('<a>').attr({href: '#split-toggle', class: 'fly-link top'}).text('Split score');
-			// var ul = $('<ul>');
-			// var block_user = $('<li>').attr({class: 'block block-user'}).append($('<input>').attr({class: 'pix-input input-user',type: 'text', placeholder: '1...'}));
-			// var block_dialogue = $('<li>').attr({class: 'block block-dialogue'}).append($('<input>').attr({class: 'pix-input input-dialogue',type: 'text', placeholder: '2...'}));
-			// var block_system = $('<li>').attr({class: 'block block-system'}).append($('<input>').attr({class: 'pix-input input-system',type: 'text', placeholder: '3...'}));
-			// var note = $('<div>').attr({class: 'note'}).append($('<input>').attr({class: 'pix-input input-note',type: 'text', placeholder: '4...'}));
-			// var fly_bottom = $('<a>').attr({href: '#add-note', class: 'fly-link bottom'}).text('Add note');
-			// ul.append(block_user).append(block_dialogue).append(block_system);
-			//var column = li.append(fly_top).append(ul).append(fly_bottom);
 			var step_template = $('#pix-step').html();
 			var column = Handlebars.compile(step_template);
 
 			$('.pix-steps').append(column).find('.pix-step').last().find('li').first().find('.pix-div-input').focus();
-			$('.pix-div-input').bind('keypress',function(event){
-				$(this).checkText($(this).text(), event);
-			});
-			$('.fly-link').bind('click',function(event){
-				$(this).flyLink();
-			});
 		}
 		var that = this;
+		/*
+			Control del tab para crear nueva columna
+		*/
 		if (event.keyCode == 9) {
 			//next tab
 			event.preventDefault();
@@ -181,6 +151,9 @@ var pixObject = {
 }(jQuery));
 
 jQuery(document).ready(function($){
+	/*
+		Handlebars
+	*/
 	var pix_layout = $('#layout-score').html();
 	var step_template = $('#pix-step').html();
 	var step_compile = Handlebars.compile(step_template);
@@ -188,18 +161,10 @@ jQuery(document).ready(function($){
 	var context = {step: step_compile};
 	var html = template(context);
 	$('#pix-template').html(html);
+
+	/*
+		Iniciamos eventos
+	*/
 	$.handleEvents.init();
-	// $('.pix-div-input').bind('keypress',function(event){
-	// 	$(this).checkText($(this).text(), event);
-	// });
-	// $('.tool-add').bind('click',function(event){
-	// 	$(this).addNodeCurrent($(this));
-	// });
-	// $('#add-new').bind('click',function(event){
-	// 	$('#pix-template').addScore();
-	// });
-	// $('.pix-div-input').bind('keyup', function(event){
-	// 	var target = $(this);
-	// 	$(this).replacePix($(this).text(), target);
-	// });
+	
 });
